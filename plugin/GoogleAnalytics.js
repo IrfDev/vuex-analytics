0270c724a9ee866de9399c1d337d748075c1c6e6
Irving Suárez <irving.dev@icloud.com>
2020-11-13 22:42:02 -0600

    As

---
+++ plugin/GoogleAnalytics.js
@@ -0,0 +1,21 @@
+export const GoogleAnalyticsPLugins = (store) => {
+  store.subscribeAction((action, state) => {
+    if (typeof action.payload.analytics === 'object' &&
+      action.payload.analytics.action &&
+      action.payload.analytics.category) {
+      if (action.payload.analytics.action === 'timing_complete') {
+         gtag('event', action.payload.analytics.action, {
+          'name' : action.payload.analytics.label || action.payload.analytics.name,
+          'event_category' : action.payload.analytics.category,
+          'value' : action.payload.analytics.value,
+      });
+      } else {
+        gtag('event', action.payload.analytics.action, {
+        'event_category': action.payload.analytics.category,
+        'event_label': action.payload.analytics.label,
+        'value': action.payload.analytics.value
+      });
+      }
+    };
+  })
+}
